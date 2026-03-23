a16f20509ca9c677ea56e3a782a9ec7600ed5f67
vinayakbeing1 <vinayakbeing1@gmail.com>
2026-03-23 18:56:37 +0530

    feast function to check beast and dish match

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def sum_array(a):
   return sum(a)
+
+
+def feast(beast, dish):
+    if beast[0].lower() == dish[0].lower() and beast[-1].lower() == dish[-1].lower():
+        return True
+    else:
+        return False
+ 
