52634950c6f4e45683234c4926e69c1b68fb7d8d
vinayakbeing1 <vinayakbeing1@gmail.com>
2026-03-24 16:41:04 +0530

    Add CTE to filter departments based on special sales

--- solution.sql
+++ solution.sql
@@ -13,3 +13,16 @@ WHERE (continent = 'Africa' or continent = 'Afrika')
   AND country LIKE 'E%'
 ORDER BY capital
 LIMIT 3;
+
+
+WITH special_sales AS (
+    SELECT *
+    FROM sales
+    WHERE price > 90
+)
+SELECT id, name
+FROM departments
+WHERE id IN (
+    SELECT department_id
+    FROM special_sales
+);
