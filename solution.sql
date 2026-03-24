c385728de1ada08d65abb06a09ea2b43bff9c663
vinayakbeing1 <vinayakbeing1@gmail.com>
2026-03-24 16:11:46 +0530

    Add query for capitals in Africa starting with 'E'
    Add query to select capitals of African countries starting with 'E'.

--- solution.sql
+++ solution.sql
@@ -5,3 +5,11 @@ from opposite;
 
 SELECT CONCAT_WS(' ', prefix, first, last, suffix) AS title
 FROM names;
+
+
+SELECT capital
+FROM countries
+WHERE (continent = 'Africa' or continent = 'Afrika') 
+  AND country LIKE 'E%'
+ORDER BY capital
+LIMIT 3;
